71a307b8ede63a1b31e1f829c0f64668dd457666
SerNike <seda.2000@inbox.ru>
2023-01-11 02:22:55 +0500

    commit done

--- src/com/skypro/HW11/Main.java
+++ src/com/skypro/HW11/Main.java
@@ -2,6 +2,30 @@ package com.skypro.HW11;
 
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1() {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " "+ lastName;
+        System.out.println("ФИО сотрудника —  " + fullName);
+    }
+    public static void task2() {
+        String firstName = "ivanov";
+        String middleName = "ivan";
+        String lastName = "ivanovich";
+        firstName = firstName.replace("i", "I");
+        middleName = middleName.replace("i", "I");
+        lastName = lastName.replace("i", "I");
+        String fullName = firstName + " " + middleName + " "+ lastName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+    }
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
     }
 }
